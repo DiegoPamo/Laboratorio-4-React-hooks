8d8c6122eb654cdb1bb7fd46fb01d864c0d733ab
Diego Pamo <diego.pamo@tecsup.edu.pe>
2021-03-30 20:13:06 -0500

    punto 1

---
+++ lab04react/src/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const GoodClick = () =>{
+    setGood(good + 1)
+  }
+  const NeutralClick = () =>{
+    setNeutral(neutral + 1)
+  }
+  const BadClick = () =>{
+    setBad(bad + 1)
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={GoodClick}>good</button>
+      <button onClick={NeutralClick}>neutral</button>
+      <button onClick={BadClick}>bad</button>
+      <div>
+        <h1>Statictics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
